bc5a3f70004aa2a73b971faa67c17f0ced0f5369
Junette <junetter@gmail.com>
2021-04-12 22:16:30 -0700

    outputting anymal states into example.txt

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,14 +5,10 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("spot_ros/spot_description")
-subdirs("spot_ros/spot_driver")
-subdirs("spot_ros/spot_viz")
 subdirs("turtlebot3/turtlebot3")
 subdirs("turtlebot3_msgs")
 subdirs("turtlebot3/turtlebot3_navigation")
 subdirs("turtlebot3_simulations/turtlebot3_simulations")
-subdirs("spot_ros/spot_msgs")
 subdirs("r2d2")
 subdirs("turtlebot3/turtlebot3_bringup")
 subdirs("turtlebot3/turtlebot3_example")
